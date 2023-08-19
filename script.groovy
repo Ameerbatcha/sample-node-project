03f4ae8d3e33322ba62eb9f2e6dcb224d179b373
Ameerbatcha <ameerbatcha.cse@gmail.com>
2023-08-19 20:45:00 +0530

    final changes done to correct the error done.

--- script.groovy
+++ script.groovy
@@ -1,9 +1,16 @@
+import groovy.transform.NonCPS
+
+@NonCPS
 def testApp() {
   echo "TEST done successfully"
 }
+
+@NonCPS
 def buildApp() {
   echo "Build generated successfully FOR VERSION "
 }
+
+@NonCPS
 def deployApp() {
   echo "Application Deployed successfully"
 }
