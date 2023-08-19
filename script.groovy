5edf07ed8bf79f514a8f4a7a80739fa82016202b
Ameer Batcha S <89926676+Ameerbatcha@users.noreply.github.com>
2023-08-19 21:04:46 +0530

    Created script.groovy

---
+++ script.groovy
@@ -0,0 +1,11 @@
+def testApp() {
+  echo "TEST done successfully"
+}
+
+def buildApp() {
+  echo "Build generated successfully FOR VERSION "
+}
+
+def deployApp() {
+  echo "Application Deployed successfully"
+}
